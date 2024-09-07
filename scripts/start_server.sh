68dca1229eacc9c521b9099de099e10daa274527
ABHISHEK SIDANA <abhishek.sidana@daimlertruck.com>
2024-09-07 14:37:52 +0530

    added stuff

--- scripts/start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Start web server (Assuming you're using Apache here)
-sudo systemctl start httpd   # For Amazon Linux 2
--- scripts/stop_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Stop web server (Assuming you're using Apache here)
-sudo systemctl stop httpd   # For Amazon Linux 2
